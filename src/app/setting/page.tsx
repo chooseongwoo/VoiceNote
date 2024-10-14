75282de055fe9974dbbdcce8ee0cf4d8519aef7b
추성우 <cntjddn715@gmail.com>
2024-10-14 19:11:27 +0900

    add :: 설정에서 정한 값 저장

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -5,15 +5,28 @@ import { useState } from "react";
 import Slider from "@/components/Slider";
 
 export default function Setting() {
-  const [speed, setSpeed] = useState(1.0);
-  const [tone, setTone] = useState(0.0);
+  const [values, setValues] = useState({
+    speed: Number(localStorage.getItem("speed")) || 1.0,
+    tone: Number(localStorage.getItem("tone")) || 0.0,
+    gender: localStorage.getItem("gender") || "남성",
+  });
 
   const handleSpeedChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSpeed(Number(event.target.value));
+    const newSpeed = Number(event.target.value);
+    setValues({ ...values, speed: newSpeed });
+    localStorage.setItem("speed", newSpeed.toString());
   };
 
   const handleToneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTone(Number(event.target.value));
+    const newTone = Number(event.target.value);
+    setValues({ ...values, tone: newTone });
+    localStorage.setItem("tone", newTone.toString());
+  };
+
+  const handleGenderChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = event.target.value;
+    setValues({ ...values, gender: selectedValue });
+    localStorage.setItem("gender", selectedValue);
   };
 
   return (
@@ -24,19 +37,19 @@ export default function Setting() {
           label="음성 속도"
           min="0.25"
           max="4.0"
-          value={speed}
+          value={values.speed}
           handleChange={handleSpeedChange}
         />
         <Slider
           label="음성 톤"
           min="-20"
           max="20"
-          value={tone}
+          value={values.tone}
           handleChange={handleToneChange}
         />
         <_.SelectContainer>
           <_.Label>음성 성별</_.Label>
-          <_.Select>
+          <_.Select value={values.gender} onChange={handleGenderChange}>
             <_.Option value="남성">남성</_.Option>
             <_.Option value="여성">여성</_.Option>
           </_.Select>
