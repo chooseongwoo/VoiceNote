37637c80cce2ef9e1e06321241dd9d9c44780697
추성우 <cntjddn715@gmail.com>
2024-10-17 00:26:43 +0900

    chore :: json 파일 절대경로

--- next.config.mjs
+++ next.config.mjs
@@ -7,21 +7,19 @@ if (process.env.GOOGLE_APPLICATION_CREDENTIALS_CONTENTS) {
     "base64"
   ).toString("utf-8");
 
-  const credentialsPath = path.join(
-    process.cwd(),
-    "keys",
-    "my-google-credentials.json"
-  );
+  const credentialsPath = path.join("/tmp", "my-google-credentials.json");
 
-  if (!fs.existsSync(path.dirname(credentialsPath))) {
-    fs.mkdirSync(path.dirname(credentialsPath), { recursive: true });
+  try {
+    if (!fs.existsSync(credentialsPath)) {
+      fs.writeFileSync(credentialsPath, credentials);
+    }
+
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
+
+    console.log("Google Credentials file created at:", credentialsPath);
+  } catch (error) {
+    console.error("Failed to create Google Credentials file:", error);
   }
-
-  if (!fs.existsSync(credentialsPath)) {
-    fs.writeFileSync(credentialsPath, credentials);
-  }
-
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
 }
 
 export default {
