66d2066f93515f4a359426a4c87a94ab0ff5aeb2
추성우 <cntjddn715@gmail.com>
2024-10-16 19:27:45 +0900

    fix :: build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+export default {
+  async redirects() {
+    return [
+      {
+        source: "/main",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
-
-export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-const Main = dynamic(() => import("./(main)/page"), { ssr: false });
+const Main = dynamic(() => import("./main/page"), { ssr: false });
 
 export default function Home() {
   return <Main />;
