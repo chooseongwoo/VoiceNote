12571925205f59e428221f9a94ca897904a233e3
추성우 <cntjddn715@gmail.com>
2024-10-16 12:20:36 +0900

    add :: splash 화면 띄우기

--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -1,7 +1,20 @@
+"use client";
 import Logo from "@/assets/Logo";
 import * as _ from "./style";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Splash() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <_.Layout>
       <Logo width="130" height="130" />
